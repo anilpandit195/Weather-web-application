d5cb6ef85329e6de12cce68f0a275d21aa472d6a
Anil Baidnath Pandit <87321054+anilpandit195@users.noreply.github.com>
2022-12-26 15:59:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const API_KEY = `8ae8f397b516b102e2fb4ba9223e1067`
+const form = document.querySelector("form")
+const search = document.querySelector("#input")
+const weather = document.querySelector("#api")
+
+    const getWeather = async(search) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    console.log(data)
+    console.log(JSON.stringify(data, null,'\t'));
+    return showWeather(data)
+};
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
+    weather.innerHTML = `
+        <div>
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+            <h2 class="tempr">${data.main.temp} ℃</h2>
+            <h6> ${data.weather[0].main} </h6>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event){
+        getWeather(search.value)
+        event.preventDefault();
+    }
+    
+)
